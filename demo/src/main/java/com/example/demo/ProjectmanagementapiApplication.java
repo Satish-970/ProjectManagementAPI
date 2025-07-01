82f932d6eea6362f0f984cc206f5ade7e226da59
satish <114747140+Satish-970@users.noreply.github.com>
2025-07-01 17:16:46 +0530

    Project Done 001

---
+++ demo/src/main/java/com/example/demo/ProjectmanagementapiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // Automatically scans com.example.demo and all subpackages
+public class ProjectmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmanagementapiApplication.class, args);
+	}
+}
