d27710f56290e1346d81f3390632aa407fdc703f
satish <114747140+Satish-970@users.noreply.github.com>
2025-07-02 09:52:11 +0530

    Update ProductRepo.java

--- demo/src/main/java/Repositry/ProductRepo.java
+++ demo/src/main/java/Repositry/ProductRepo.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
+@Repositry
 public class ProductRepo {
     private List<Product> products = new ArrayList<>();
    private long currentId = 1;
