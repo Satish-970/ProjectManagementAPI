3294c91b44eaf29002d2c9ab23fb988fb2dff028
satish <114747140+Satish-970@users.noreply.github.com>
2025-07-02 09:51:46 +0530

    Update ProjectmanagementapiApplication.java

--- demo/src/main/java/com/example/demo/ProjectmanagementapiApplication.java
+++ demo/src/main/java/com/example/demo/ProjectmanagementapiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication  // Automatically scans com.example.demo and all subpackages
+@ComponentScan(basePackages = "controllayer, Service, Repositry")
+// Explicitly include the packages where your controllers, services, and repositories are located
 public class ProjectmanagementapiApplication {
 
 	public static void main(String[] args) {
